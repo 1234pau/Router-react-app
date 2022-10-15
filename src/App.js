16b5da59904e81b08c295720f15c5927bc94cd40
paul geana <87538456+1234pau@users.noreply.github.com>
2022-10-15 17:49:19 +0300

    create custom hooks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {Route, Routes, useNavigate} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import {format} from 'date-fns'
 import api from'./api/posts'
+import useAxiosFetch from './hooks/useAxiosFetch'
 
 function App() {
 const [posts, setPosts]=useState([])
@@ -20,26 +21,33 @@ const [editTitle, setEditTitle] = useState('');
 const [editBody, setEditBody] = useState('');
 const navigate = useNavigate();
 
+const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts')
+
+
+// useEffect(()=>{
+// const fetchPosts = async ()=>{
+//   try{
+//     const response = await api.get('/posts')
+//     setPosts(response.data)
+
+//   }catch (err){
+//     if (err.response) {
+//       // Not in the 200 response range 
+//       console.log(err.response.data);
+//       console.log(err.response.status);
+//       console.log(err.response.headers);
+//     } else {
+//       console.log(`Error: ${err.message}`);
+//     }
+
+//   }
+// }
+// fetchPosts()
+// }, [])
+
 useEffect(()=>{
-const fetchPosts = async ()=>{
-  try{
-    const response = await api.get('/posts')
-    setPosts(response.data)
-
-  }catch (err){
-    if (err.response) {
-      // Not in the 200 response range 
-      console.log(err.response.data);
-      console.log(err.response.status);
-      console.log(err.response.headers);
-    } else {
-      console.log(`Error: ${err.message}`);
-    }
-
-  }
-}
-fetchPosts()
-}, [])
+  setPosts(data)
+}, [data])
 
 useEffect(() => {
   const filteredResults = posts.filter((post) =>
@@ -100,7 +108,10 @@ const handleDelete = async (id)=>{
         search={search}
         setSearch={setSearch}/>}>
         <Route index element={<Home 
-          posts={searchResults}/>}/>
+          posts={searchResults}
+          fetchError={fetchError}
+          isLoading={isLoading}
+          />}/>
         <Route path='post'>
           <Route index element={<NewPost 
             handleSubmit={handleSubmit}
